2611eb1f1698130dfa37757c84b3944a2c6c0a66
Robby Tan <robbytan@181-181.priv29.nus.edu.sg>
2016-05-03 16:20:08 +0800

    message

--- bootstrap-3.3.6-dist/js/load.js
+++
@@ -1,7 +0,0 @@
-function loadPage(href)
-        {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.open("GET", href, false);
-            xmlhttp.send();
-            return xmlhttp.responseText;
-        }
---
+++ header.php
@@ -0,0 +1,23 @@
+<header role="banner" class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
+  <div class="container">
+
+    <nav role="navigation" class="collapse navbar-collapse bs-navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="./getting-started">Getting started</a>
+        </li>
+
+        <li>
+          <a href="./components">Components</a>
+        </li>
+        <li>
+          <a href="./javascript">JavaScript</a>
+        </li>
+        <li>
+          <a href="./customize">Customize</a>
+        </li>
+      </ul>
+
+    </nav>
+  </div>
+</header>
